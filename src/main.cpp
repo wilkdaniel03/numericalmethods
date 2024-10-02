3926bbfc2165f6c3b09a9721d5e071d4ffd949c4
wilkdaniel03 <wilkdaniel03@gmail.com>
2024-10-02 17:55:27 +0200

    Improved printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include "../include/taylor.h"
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 static double PI = 3.141592653;
 
+typedef double (*funcint)(int);
+typedef double (*funcdouble)(double);
+
+void printint(funcint f, std::string str, int arg) {
+	std::cout << std::setw(10) << std::left << str;
+	std::cout << std::setw(10) << std::setprecision(10) << std::right << f(arg);
+	std::cout << std::endl;
+}
+
+void printdouble(funcdouble f, std::string str, double arg) {
+	std::cout << std::setw(10) << std::left << str;
+	std::cout << std::setw(10) << std::setprecision(10) << std::right << f(arg);
+	std::cout << std::endl;
+}
+
 int main(void) {
-	std::cout << std::setprecision(10) << Taylor::euler(1) << std::endl;
-	std::cout << std::setprecision(10) << Taylor::sine(30. * PI / 180.) << std::endl;
-	std::cout << std::setprecision(10) << Taylor::cosine(60 * PI / 180.) << std::endl;
+	printint(Taylor::euler, std::string("Euler"),1);
+	printdouble(Taylor::sine, std::string("Sine"),30. * PI / 180.);
+	printdouble(Taylor::cosine, std::string("Cosine"),60. * PI / 180.);
 
 	return 0;
 }
